3745eaefef24a4b04403de21c0e659e13950d716
Hel Mab <mabhelitc@gmail.com>
2020-09-29 12:19:12 +0700

    [TASK] Add vue-i18n

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,6 +29,7 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
+    '~/plugins/i18n.js'
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
---
+++ plugins/i18n.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import VueI18n from 'vue-i18n'
+
+Vue.use(VueI18n)
+
+const files = require.context('../locales/', false, /\.json$/i)
+const messages = {}
+files.keys().forEach((key) => {
+  const lang = key.split('/')[1].split('.')[0]
+  messages[lang] = files(key)
+})
+
+export default ({ app, store }) => {
+  app.i18n = new VueI18n({
+    locale: 'en',
+    fallbackLocale: 'en',
+    messages
+  })
+}
