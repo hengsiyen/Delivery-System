6e88a7540393995fdfa7b6bfa46344f1b592b968
H.Siyen <hengsiyen@gmail.com>
2021-09-23 21:02:05 +0700

    update issues DS-2

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
     title: 'Delivery System',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, height=device-height, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
